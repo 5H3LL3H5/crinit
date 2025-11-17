5802b8933f0ea904ba75b348e163b5fc496f2a53
Friedrich Schwedler <fschwedler@emlix.com>
2025-11-17 11:59:11 +0100

    feat: find next timestamp timer should trigger

--- inc/timer.h
+++ inc/timer.h
@@ -37,23 +37,52 @@ void crinitTimerSetDefault(crinitTimerDef_t *td);
 
 /**
  * Check if a timer definition gives a valid timer
+ * ie. month between 1 and 12, day in 1 to 31, hours 0 to 23, ...
+ *
+ * @param td  the timer definition to set check
+ *
+ * @return true if the timer definition is valid, and false if not
  */
 bool crinitCheckTimerDef(crinitTimerDef_t *td);
 
 /**
  * Check if a value is in a closed-open range (test in [start, end) ) ie start <= test < end.
+ *
+ * @param start   the lower end of the range to check
+ * @param test    the value to check if it's in range
+ * @param end     the upper end of the range to check
+ *
+ * @return true if test is higher or equal to start and lower then end
  */
 #define CO_RANGE(start, test, end) (((start) <= (test)) && ((test) < (end)))
 /**
  * Check if a value is in a closed-closed range (test in [start, end] ) ie start <= test <= end.
+ *
+ * @param start   the lower end of the range to check
+ * @param test    the value to check if it's in range
+ * @param end     the upper end of the range to check
+ *
+ * @return true if test is higher or equal to start and lower or equal to end
  */
 #define CC_RANGE(start, test, end) (((start) <= (test)) && ((test) <= (end)))
 /**
  * Check if a value is in a open-closed range (test in (start, end] ) ie start < test <= end.
+ *
+ * @param start   the lower end of the range to check
+ * @param test    the value to check if it's in range
+ * @param end     the upper end of the range to check
+ *
+ * @return true if test is higher then start and lower or equal to end
  */
 #define OC_RANGE(start, test, end) (((start) < (test)) && ((test) <= (end)))
 /**
  * Check if a value is in a open-open range (test in (start, end) ) ie start < test < end.
+ *
+ * @param start   the lower end of the range to check
+ * @param test    the value to check if it's in range
+ * @param end     the upper end of the range to check
+ *
+ * @return true if test is higher then start and lower then end
  */
 #define OO_RANGE(start, test, end) (((start) < (test)) && ((test) < (end)))
 
@@ -74,7 +103,7 @@ int crinitSPrintTimerDef(char *s, crinitTimerDef_t *td);
 void crinitPrintTimerDef(crinitTimerDef_t *td);
 
 /**
- * Adds a timer to crinit
+ * Adds a timer to crinits timer pool.
  *
  * @param timerStr  the configuration string for the timer
  */
@@ -88,5 +117,35 @@ void crinitTimerAdd(char *timerStr);
  * @return true on success, false otherwise
  */
 bool crinitTimerParse(char *s, crinitTimerDef_t *td);
+
+/**
+ * Calculate the next time the timer should trigger.
+ *
+ * @param last  the last timestamp to calculate the next from
+ * @param td    the timer definition to calculate the next time from
+ *
+ * @return the timestamp the timer is fullfiled next
+ */
+struct timespec crinitTimerNextTime(struct timespec *last, crinitTimerDef_t *td);
+
+/**
+ * Get a `struct tm` similar to `gmtime_r` with a specific timezone.
+ *
+ * @param time      the timestamp to transform into a tm
+ * @param timezone  the timezone with hour and minute
+ * @param result    the resulting tm struct
+ *
+ * @return  a tm representation in the specyfied timezone
+ */
+struct tm *crinitZonedTimeR(const time_t *time, int8_t timezone[2], struct tm *restrict result);
+/**
+ * Check if a timerstamp is valid for a timer
+ *
+ * @param ts  the timestamp to check.
+ * @param td  the crinit timer definition to check against
+ *
+ * @return true if the timer should trigger at that timestamp, false otherwise
+ */
+bool crinitCheckTimerTime(struct timespec ts, crinitTimerDef_t *td);
 
 #endif  // __TIMER_H__
--- src/timer.c
+++ src/timer.c
@@ -22,13 +22,58 @@
  */
 static bool crinitIsLeapYear(uint16_t year);
 /**
- * get a static srting with the short month name
+ * get a static string with the short month name
  *
- * @param m  the month starting at 1 for jan
+ * @param m  the month starting at 1 for Jan
  *
  * @return "Jan" for 1, "Feb" for 2, ...
  */
 static const char *crinitMonthToStr(uint8_t m);
+/**
+ * Get the number of days a month is long.
+ *
+ * @param month  the month to get the length of
+ * @param year   the year to know if Feb is 28 or 29 days long
+ *
+ * @return the number of days the month is long
+ */
+static int crinitMonthLength(uint8_t month, uint16_t year);
+/**
+ * Get the next value for a range.
+ *
+ * @param last      the value to get the next from
+ * @param interval  the interval in which the next needs to be
+ * @param start     the lowest possible value
+ * @param limit     the highest possible value
+ *
+ * @param res: next value after last
+ * @param off: the offset to the last value
+ *
+ * @return true if the res is after last and false if the next interval needs to be updated as well
+ */
+static bool crinitNext(int last, uint8_t interval[2], int start, int limit, int *res, long long int *offs);
+/**
+ * get the next year
+ *
+ * @param last      the year from which to start finding the next
+ * @param interval  the interval in which the next year needs to be in
+ *
+ * @param res   the resulting next year
+ * @param offs  the offset the next year has to the last
+ *
+ * @return true if a next year is found/possible in the interval, false if not
+ */
+static bool crinitNextYear(int last, uint16_t interval[2], int *res, long long int *offs);
+/**
+ * Returns the number of days from start of the year to the first of the specified month
+ * ie. 0 for 1 (Jan), 31 for 2 (Feb), 59 (or 60 in a leap year) for 3 (Mar) and so on
+ *
+ * @param m     the month to get how many days the year had before the fist of it
+ * @param year  the year to check in
+ *
+ * @return  the number of days the year had before the first of that month
+ */
+static int crinitDaysToMonth(uint8_t m, uint16_t year);
 
 void crinitTimerSetDefault(crinitTimerDef_t *td) {
     td->wDay = 0x7f;
@@ -162,11 +207,255 @@ bool crinitCheckTimerDef(crinitTimerDef_t *td) {
     return res;
 }
 
+bool crinitCheckTimerTime(struct timespec ts, crinitTimerDef_t *td) {
+    struct tm t;
+    crinitZonedTimeR(&ts.tv_sec, td->timezone, &t);
+    uint8_t mon = t.tm_mon + 1;
+    uint16_t year = t.tm_year + 1900;
+    uint8_t wDay = 1 << ((t.tm_wday + 6) % 7);
+    return (CC_RANGE(td->seconds[0], t.tm_sec, td->seconds[1]) || CO_RANGE(t.tm_sec, td->seconds[1], td->seconds[0]) ||
+            OC_RANGE(td->seconds[1], td->seconds[0], t.tm_sec)) &&
+           (CC_RANGE(td->minutes[0], t.tm_min, td->minutes[1]) || CO_RANGE(t.tm_min, td->minutes[1], td->minutes[0]) ||
+            OC_RANGE(td->minutes[0], td->minutes[1], t.tm_min)) &&
+           (CC_RANGE(td->hours[0], t.tm_hour, td->hours[1]) || CO_RANGE(t.tm_hour, td->hours[1], td->hours[0]) ||
+            OC_RANGE(td->hours[1], td->hours[0], t.tm_hour)) &&
+           (CC_RANGE(td->days[0], t.tm_mday, td->days[1]) || CO_RANGE(t.tm_mday, td->days[1], td->days[0]) ||
+            OC_RANGE(td->days[1], td->days[0], t.tm_mday)) &&
+           (CC_RANGE(td->month[0], mon, td->month[1]) || CO_RANGE(mon, td->month[1], td->month[0]) ||
+            OC_RANGE(td->month[1], td->month[0], mon)) &&
+           (CC_RANGE(td->years[0], year, td->years[1]) || CO_RANGE(year, td->years[1], td->years[0]) ||
+            OC_RANGE(td->years[1], td->years[0], year)) &&
+           (wDay & td->wDay);
+}
+
+static const char *crinitDayLookup[] = {
+    [0] = "Su", [1] = "Mo", [2] = "Tu", [3] = "We", [4] = "Th", [5] = "Fr", [6] = "Sa",
+};
+
 void crinitTimerAdd(char *timerStr) {
     crinitInfoPrint("@timer:%s", timerStr);
     crinitTimer_t timer = {0};
     timer.name = strdup(timerStr);
     crinitTimerParse(timer.name, &(timer.def));
     crinitPrintTimerDef(&(timer.def));
+
+    struct timespec ti;
+    timespec_get(&ti, TIME_UTC);
+
+    char timeBuff[100];
+    char utcBuff[100];
+    struct tm t = {0};
+    struct tm utc = {0};
+
+    gmtime_r(&ti.tv_sec, &utc);
+    crinitZonedTimeR(&ti.tv_sec, timer.def.timezone, &t);
+    strftime(utcBuff, 100, "%F %H:%M:%S UTC", &utc);
+    strftime(timeBuff, 100, "%F %H:%M:%S %z", &t);
+    crinitDbgInfoPrint(" -  %s %s (%s)", crinitDayLookup[t.tm_wday], timeBuff, utcBuff);
+    for (size_t n = 1; n <= 20; n++) {
+        ti = crinitTimerNextTime(&ti, &(timer.def));
+        if (ti.tv_sec == 0 && ti.tv_nsec == 0) {
+            crinitDbgInfoPrint(" -  No next time found!!");
+            break;
+        }
+        gmtime_r(&ti.tv_sec, &utc);
+        crinitZonedTimeR(&ti.tv_sec, timer.def.timezone, &t);
+        strftime(utcBuff, 100, "%F %H:%M:%S UTC", &utc);
+        strftime(timeBuff, 100, "%F %H:%M:%S %z", &t);
+        crinitDbgInfoPrint("%2lu) %s %s (%s)", n, crinitDayLookup[t.tm_wday], timeBuff, utcBuff);
+    }
+
     // TODO:  initialize, start timerfd and polling in timer thread
+}
+
+static int crinitMonthLength(uint8_t month, uint16_t year) {
+    switch (month) {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            return 31;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            return 30;
+        case 2:
+            return crinitIsLeapYear(year) ? 29 : 28;
+        default:
+            return -1;
+    }
+}
+
+static bool crinitNext(int last, uint8_t interval[2], int start, int limit, int *res, long long int *offs) {
+    uint8_t range[2] = {start <= interval[0] ? interval[0] : start, interval[1] <= limit ? interval[1] : limit};
+    long long int off = 0;
+    bool done = false;
+    if (range[0] == range[1]) {
+        // if its a single possible value set the offset to the difference
+        // what amount needs to change to get to that value
+        off += (range[0] - last);
+    } else if (range[0] < range[1]) {
+        // a "standard" range of start < end
+        if (CO_RANGE(range[0], last, range[1])) {
+            // if last is in the range [start, end) it just 1 needs to be added
+            off += 1;
+        } else {
+            // if not next needs to be set to start
+            off += (range[0] - last);
+        }
+    } else if (range[0] > range[1]) {
+        // if the range wraps around
+        // ie is a range from start to range[1] and one from range[0] to limit
+        if (CO_RANGE(range[1], last, range[0])) {
+            // last is in the not allowed interval between range[1] and range[0]
+            // set next to range[0]
+            off += (range[0] - last);
+        } else if (last == limit) {
+            // last is the limit set next to start
+            off -= (limit - start);
+        } else {
+            // otherwise just 1 can be added
+            off += 1;
+        }
+    }
+    if (off > 0) {
+        // if the offset is positive the next value can be found
+        // within the range without rolling over
+        done = true;
+    }
+    *offs = off;
+    *res = last + off;
+    return done;
+}
+
+static bool crinitNextYear(int last, uint16_t interval[2], int *res, long long int *offs) {
+    if (interval[0] == interval[1]) {
+        crinitErrPrint("was only supposed to run in the year %d", last);
+        return false;
+    } else if (interval[0] < interval[1]) {
+        if (CO_RANGE(interval[0], last, interval[1])) {
+            *res = last + 1;
+            *offs = 1;
+            return true;
+        } else {
+            crinitErrPrint("%d was the last possible year", last);
+            return false;
+        }
+    } else {
+        if (last < interval[1]) {
+            *res = last + 1;
+            *offs = 1;
+            return true;
+        } else if (last >= interval[0]) {
+            *offs = 1;
+            *res = last + 1;
+            return true;
+        } else {
+            *res = interval[0];
+            *offs = interval[0] - last;
+            crinitErrPrint("something super weird with the year %d", last);
+            return false;
+        }
+    }
+    return false;
+}
+
+static const int crinitDayToMonthLookup[] = {
+    [1] = 0,   [2] = 31,  [3] = 59,  [4] = 90,   [5] = 120,  [6] = 151,
+    [7] = 181, [8] = 212, [9] = 243, [10] = 273, [11] = 304, [12] = 334,
+};
+
+static int crinitDaysToMonth(uint8_t m, uint16_t year) {
+    if (m != 0 && m <= 2) {
+        return crinitDayToMonthLookup[m];
+    } else if (m <= 12) {
+        return crinitDayToMonthLookup[m] + crinitIsLeapYear(year);
+    } else {
+        return -1;
+    }
+}
+
+struct tm *crinitZonedTimeR(const time_t *time, int8_t timezone[2], struct tm *restrict result) {
+    long off = (long)timezone[0] * 3600 + (long)timezone[1] * 60;
+    time_t tmpT = (*time) + off;
+    gmtime_r(&tmpT, result);
+    result->tm_gmtoff = off;
+    return result;
+}
+
+struct timespec crinitTimerNextTime(struct timespec *last, crinitTimerDef_t *td) {
+    time_t t = last->tv_sec;
+    struct tm times = {0};
+    crinitZonedTimeR(&t, td->timezone, &times);
+
+    struct timespec next = {0};
+
+    // tells if a next time is already found
+    bool done = false;
+
+    long long int offSec = 0;
+    long long int offMin = 0;
+    long long int offHour = 0;
+    long long int offDays = 0;
+
+    long long int off = 0;
+    int partRes = 0;
+    // update seconds
+    done = crinitNext(times.tm_sec, td->seconds, 0, 59, &partRes, &offSec);
+    if (!done || !CC_RANGE(td->minutes[0], times.tm_min, td->minutes[1])) {
+        // update minutes only if last time wasn't a valid timestamp or no valid time was found yet
+        // if a valid next second is still at an invalid minute the whole timestamp is still invalid
+        done = crinitNext(times.tm_min, td->minutes, 0, 59, &partRes, &offMin);
+    }
+    if (!done || !CC_RANGE(td->hours[0], times.tm_hour, td->hours[1])) {
+        done = crinitNext(times.tm_hour, td->hours, 0, 23, &partRes, &offHour);
+    }
+    int day = times.tm_mday;
+    int month = times.tm_mon + 1;
+    int year = times.tm_year + 1900;
+    int tries = 0;
+    do {
+        if (!done || !CC_RANGE(td->days[0], day, td->days[1])) {
+            int last = day;
+            // special case needed for Feb 29 to stay on Feb 29 and just update the year
+            if (td->days[0] == 29 && td->days[1] == 29 && td->month[0] == 2 && td->month[1] == 2 && day == 29) {
+                done = false;
+            } else {
+                int ml = crinitMonthLength(month, year);
+                done = crinitNext(last, td->days, 1, ml, &day, &off);
+                offDays += off;
+            }
+        }
+        if (!done || !CC_RANGE(td->month[0], month, td->month[1])) {
+            int last = month;
+            done = crinitNext(last, td->month, 1, 12, &month, &off);
+            offDays += crinitDaysToMonth(month, year) - crinitDaysToMonth(last, year);
+        }
+        if (!done || !CC_RANGE(td->years[0], year, td->years[1])) {
+            int last = year;
+            done = crinitNextYear(last, td->years, &year, &off);
+            offDays += off * (365 + (crinitIsLeapYear(last) ? 1 : 0));
+        }
+        if (!done) {
+            crinitErrPrint("No possible next time found for timer");
+            break;
+        }
+        if (done) {
+            next.tv_nsec = last->tv_nsec;
+            next.tv_sec = last->tv_sec + offSec + offMin * 60 + offHour * 3600 + offDays * 86400;
+            done = crinitCheckTimerTime(next, td);
+        }
+        // because we don't consider the weekday when updating the day/month/year
+        // we repeat that step until everything including the weekday is valid
+        // or a limit of tries is reached that was enough reliably find Feb 29 for individual weekdays
+    } while (!done && tries++ <= 40);
+    if (!done) {
+        next.tv_sec = 0;
+        next.tv_nsec = 0;
+    }
+    return next;
 }
